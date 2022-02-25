6ec0a008c4c96bab953d5a49282bb842f5e44092
ganghye <ganghyepark@mz.co.kr>
2022-02-25 09:44:02 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
+import React, {useState} from 'react'; 
 import './App.css';
-import { Button, Card, Nav } from 'react-bootstrap';
 
 function App() {
-  return(
-    <Card>
-  <Card.Header>
-    <Nav variant="tabs" defaultActiveKey="#first">
-      <Nav.Item>
-        <Nav.Link href="#first">Active</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="#link">Link</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="#disabled" disabled>
-          Disabled
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
-  </Card.Header>
-  <Card.Body>
-    <Card.Title>Special title treatment</Card.Title>
-    <Card.Text>
-      With supporting text below as a natural lead-in to additional content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-  )
+  let [inputBox, setInputBox] = useState('');
+  return (
+    <div className="App">
+       <div className='black-nav'>
+        <div>Looker Temp webpage</div>
+      </div>
+
+      <input onChange={ (e)=>{ setInputBox(e.target.value) } } />
+      <button>confirm</button>
+
+      <div> <br/><hr/> 👇 TEMP AREA 👇 </div>
+      <div>input value : {inputBox}</div>
+     
+
+
+
+
+    </div>
+  );
 }
 
 export default App;
