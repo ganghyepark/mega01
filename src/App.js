e955ac50b215ffe0a772af616625edb313ac8017
ganghye <ganghyepark@mz.co.kr>
2022-02-25 17:43:59 +0900

    feat: dashboard 삽입

--- src/App.js
+++ src/App.js
@@ -13,11 +13,13 @@ function App() {
       <button>confirm</button>
 
       <div> <br/><hr/> 👇 TEMP AREA 👇 </div>
-      <div>input value : {inputBox}</div>
-     
-
-
-
+      <iframe id="inlineFrameExample"
+        title="Inline Frame Example"
+        width="500"
+        height="300"
+        src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik">
+        // src='Looker주소 입력'
+    </iframe>
 
     </div>
   );
